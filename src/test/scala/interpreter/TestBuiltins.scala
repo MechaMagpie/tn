d431a33b7e46bf0ded6db1e5d54a4d20d5b289cd
Mecha_Magpie <dat13egr@student.lu.se>
2017-02-24 14:07:55 +0100

    Improved file tests

--- src/test/scala/interpreter/TestBuiltins.scala
+++ src/test/scala/interpreter/TestBuiltins.scala
@@ -5,11 +5,24 @@ import java.nio.file.{Files, Paths}
 import main.scala.interpreter._
 
 import collection.mutable.Stack
+import collection.JavaConversions._
 import TestHelpers._
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSuite}
-import org.scalatest.Assertions._
 
 class TestBuiltins extends FunSuite with BeforeAndAfterAll with BeforeAndAfter {
+  val baseDir = "src/test/resources/"
+  val basePath = Paths.get(baseDir)
+
+  def provideFile(name: String, content: String) = {
+    val path = basePath.resolve(name)
+    Files.createFile(path)
+    Files.write(path, content.map(_.toByte).toArray)
+  }
+
+  override def afterAll() = {
+    for(file <- Files.newDirectoryStream(basePath))
+      Files.deleteIfExists(file)
+  }
 
   after {
     State.files.clear
@@ -17,20 +30,21 @@ class TestBuiltins extends FunSuite with BeforeAndAfterAll with BeforeAndAfter {
   }
 
   test("Input must close files after using them") {
+    provideFile("empty.tn", "")
     giveLine(" ")
-    leavesStack(TnList(Input))("src/test/resources/empty.tn")()
+    leavesStack(TnList(Input))(baseDir + "empty.tn")()
     Parser.parseNext()
     Parser.parseNext()
-    assert(State.files == Stack(DummyFile(" "), StdInReader))
+    assert(State.files === Stack(DummyFile(" "), StdInReader))
   }
 
   test("Input should make the parser parse given file") {
-    leavesStack(
-      TnList(Input, Pull, Pop, Uncons, Pop, Pull, Pop, Uncons, Pop))("src/test/resources/test1.tn")(IsEmpty, NullList)
+    provideFile("test1.tn", "[]null?")
+    leavesStack(TnList(Input, Pull, Pop, Uncons, Pop, Pull, Pop, Uncons, Pop))(baseDir + "test1.tn")(IsEmpty, NullList)
   }
 
   test("Output should allow writing to a file") {
-    val path = "src/test/resources/testtemp.txt"
+    val path = baseDir + "testtemp.txt"
     leavesStack(TnList(Output, "testing", State.table.map("print"), I))(path)()
     assert(Files.readAllBytes(Paths.get(path)).map(_.toChar).mkString("") === "testing")
     Files.delete(Paths.get(path))
